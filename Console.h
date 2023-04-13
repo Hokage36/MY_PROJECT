a72f89d65ab1543f9d12bfab44c12cf9117b3e37
Hokage36 <xuansangtruongthi2201@gmail.com>
2023-04-13 21:59:31 +0700

    my commit

--- Console.h
+++ Console.h
@@ -14,17 +14,3 @@ void setBackgroundColor(WORD color);
 void setColorBGTextXY(SHORT x, SHORT y, WORD color, WORD background, LPSTR str, ...);
 void Cursor(BOOL bVisible = TRUE, DWORD dwSize = 25);
 void deleteRow(SHORT SStartPos, SHORT SNumberRow);
-void Tao_mau_o(int x , int y , int t_color , int b_color , string name)
-{
-	setBackgroundColor(b_color);
-	int len = name.length();
-	setColor(t_color);
-	for(int i = 0 ; i < len ; ++i)
-	{
-		gotoXY(x + i, y);
-		cout << " ";
-	}
-	gotoXY(x , y) ; cout << name;
-	setBackgroundColor(1);
-}
-
